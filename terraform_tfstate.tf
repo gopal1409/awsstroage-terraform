2ba801cca7c59c06b50eb909aa23f795c424ca7f
John Cena <John@gmail.com>
2021-04-19 05:14:32 -0600

    with vpc

--- terraform_tfstate.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-    backend "s3" {
-        encrypt = true
-        bucket = "terrform-mystate-gopal"
-        key= "deploy-stage/terraform.tfstate"
-        region = "us-east-2"
-        dynamodb_table = "terraform-state"
-    }
-}
--- variables.tf
+++ variables.tf
@@ -8,7 +8,7 @@ variable "ssh_port" {
     default = 22
 }
 variable "my_system" {
-    default = "98.207.180.245/32"
+    default = "0.0.0.0/0"
 }
 
 variable "ami" {
